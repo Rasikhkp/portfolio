1d688bd914a17524c55305fa556e2dd3eca1d560
rasikhkp <raxiest@gmail.com>
2023-11-16 07:24:25 +0700

    build: configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IoMdMenu } from "react-icons/io";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div className="bg-[#111111] w-full h-screen">
+				<div className="absolute w-[53px] h-[53px] rounded-[6px] bg-[#252525] right-[14px] top-[14px] flex items-center justify-center">
+					<IoMdMenu />
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
