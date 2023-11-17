faa3e758b8594df58cea567025348d0a998b8844
rasikhkp <raxiest@gmail.com>
2023-11-17 11:14:41 +0700

    feat: Get In Touch page for tablet screen

--- src/pages/GetInTouch.tsx
+++ src/pages/GetInTouch.tsx
@@ -8,18 +8,33 @@ import CTABtn from "../components/CTABtn";
 
 const GetInTouch = () => {
 	return (
-		<div className="bg-[#111] font-poppins text-white h-fit pb-32">
-			<div className="w-full px-8 flex justify-between items-center h-16 bg-[#252525]">
+		<div className="bg-[#111] font-poppins text-white h-fit pb-32 min-[577px]:pt-20">
+			<div className="w-full min-[577px]:hidden px-8 flex justify-between items-center h-16 bg-[#252525]">
 				<div className="text-3xl font-bold text-white">
 					GET IN <span className="text-[#FFB400]">TOUCH</span>
 				</div>
 				<img className=" w-7" src={menu} alt="" />
 			</div>
 
-			<div className="px-8 mt-8">
-				<div className="text-[20px] font-medium">DON'T BE SHY!</div>
+			{/* Floating menu button */}
+			<div className="absolute hidden min-[577px]:flex transition-all w-[53px] h-[53px] rounded-[6px] bg-[#252525] right-8 top-8 items-center justify-center">
+				<img className="w-7" src={menu} alt="" />
+			</div>
 
-				<div className="mt-3 text-xs leading-relaxed tracking-wide">
+			{/* Large title */}
+			<div className="hidden min-[577px]:block text-[120px] relative font-bold text-[#222222] text-center">
+				CONTACT
+				<div className="text-[56px] absolute w-[500px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 font-bold text-white">
+					GET IN <span className="text-[#FFB400] ">TOUCH</span>
+				</div>
+			</div>
+
+			<div className="px-8 mt-8">
+				<div className="text-[20px] min-[577px]:text-2xl font-medium">
+					DON'T BE SHY!
+				</div>
+
+				<div className="mt-3 text-xs min-[577px]:text-base leading-relaxed tracking-wide">
 					Feel free to get in touch with me. I am always open to discussing
 					new projects, creative ideas or opportunities to be part of your
 					visions.
@@ -30,16 +45,16 @@ const GetInTouch = () => {
 				<div className="flex gap-4">
 					<img src={mail} alt="" />
 					<div>
-						<div className="text-xs text-[#BBBBBB]">MAIL ME</div>
-						<div className="text-xs mt-1">rasikhonly@gmail.com</div>
+						<div className="text-sm text-[#BBBBBB]">MAIL ME</div>
+						<div className="text-sm mt-1">rasikhonly@gmail.com</div>
 					</div>
 				</div>
 
 				<div className="flex gap-4 mt-7">
 					<img src={phone} alt="" />
 					<div>
-						<div className="text-xs text-[#BBBBBB]">CALL ME</div>
-						<div className="text-xs mt-1">+62 882 7936 5062</div>
+						<div className="text-sm text-[#BBBBBB]">CALL ME</div>
+						<div className="text-sm mt-1">+62 882 7936 5062</div>
 					</div>
 				</div>
 			</div>
@@ -56,17 +71,38 @@ const GetInTouch = () => {
 				</div>
 			</div>
 
-            <div className="mt-8 px-8">
-				<input className="bg-[#252525] rounded-full py-3 px-8 w-full text-sm" type="text" placeholder="YOUR NAME" />
-				<input className="bg-[#252525] rounded-full py-3 px-8 w-full text-sm mt-7" type="text" placeholder="YOUR EMAIL" />
-				<input className="bg-[#252525] rounded-full py-3 px-8 w-full text-sm mt-7" type="text" placeholder="YOUR SUBJECT" />
-				<input className="bg-[#252525] rounded-full py-3 px-8 w-full text-sm mt-7" type="text" placeholder="YOUR MESSAGE" />
-				<textarea className="bg-[#252525] rounded-[21px] py-3 px-8 w-full text-sm mt-7" placeholder="YOUR MESSAGE" name="" id="" cols={30} rows={10} ></textarea>
+			<div className="mt-8 px-8">
+				<input
+					className="bg-[#252525] rounded-full py-4 px-8 w-full text-sm"
+					type="text"
+					placeholder="YOUR NAME"
+				/>
+				<input
+					className="bg-[#252525] rounded-full py-4 px-8 w-full text-sm mt-7"
+					type="text"
+					placeholder="YOUR EMAIL"
+				/>
+				<input
+					className="bg-[#252525] rounded-full py-4 px-8 w-full text-sm mt-7"
+					type="text"
+					placeholder="YOUR SUBJECT"
+				/>
+				<input
+					className="bg-[#252525] rounded-full py-4 px-8 w-full text-sm mt-7"
+					type="text"
+					placeholder="YOUR MESSAGE"
+				/>
+				<textarea
+					className="bg-[#252525] rounded-[21px] py-4 px-8 w-full text-sm mt-7"
+					placeholder="YOUR MESSAGE"
+					name=""
+					id=""
+					cols={30}
+					rows={10}
+				></textarea>
 			</div>
 
-			<CTABtn className="mt-[75px] ml-8">
-				SEND MESSAGES
-			</CTABtn>
+			<CTABtn className="mt-[75px] ml-8">SEND MESSAGES</CTABtn>
 		</div>
 	);
 };
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,32 +4,56 @@ import porto1 from "../../public/img/1710.jpg";
 const Portfolio = () => {
 	return (
 		<div className="bg-[#111] font-poppins min-[577px]:pt-20">
+			{/* Top navigation bar */}
 			<div className="w-full min-[577px]:hidden px-8 flex justify-between items-center h-16 bg-[#252525]">
 				<div className="text-3xl font-bold text-white">
 					MY <span className="text-[#FFB400]">PORTFOLIO</span>
 				</div>
-				<img className=" w-7" src={menu} alt="" />
+				<img className="w-7" src={menu} alt="" />
 			</div>
 
+			{/* Floating menu button */}
 			<div className="absolute hidden min-[577px]:flex transition-all w-[53px] h-[53px] rounded-[6px] bg-[#252525] right-8 top-8 items-center justify-center">
 				<img className="w-7" src={menu} alt="" />
 			</div>
 
+			{/* Large title */}
 			<div className="hidden min-[577px]:block text-[120px] relative font-bold text-[#222222] text-center">
 				WORKS
 				<div className="text-[56px] absolute w-[500px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 font-bold text-white">
 					MY <span className="text-[#FFB400] ">PORTFOLIO</span>
 				</div>
 			</div>
-            
-            <div className="p-8 min-[577px]:grid min-[577px]:grid-cols-2 gap-8">
-                <img className="rounded-lg" src={porto1} alt="" />
-                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
-                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
-                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
-                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
-                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
-            </div>
+
+			{/* Image grid */}
+			<div className="p-8 min-[577px]:grid min-[577px]:grid-cols-2 gap-8">
+				<img className="rounded-lg" src={porto1} alt="" />
+				<img
+					className="rounded-lg mt-8 min-[577px]:mt-0"
+					src={porto1}
+					alt=""
+				/>
+				<img
+					className="rounded-lg mt-8 min-[577px]:mt-0"
+					src={porto1}
+					alt=""
+				/>
+				<img
+					className="rounded-lg mt-8 min-[577px]:mt-0"
+					src={porto1}
+					alt=""
+				/>
+				<img
+					className="rounded-lg mt-8 min-[577px]:mt-0"
+					src={porto1}
+					alt=""
+				/>
+				<img
+					className="rounded-lg mt-8 min-[577px]:mt-0"
+					src={porto1}
+					alt=""
+				/>
+			</div>
 		</div>
 	);
 };
