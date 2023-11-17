264e315a1cadfcdcffebc4592c4c94170b682986
rasikhkp <raxiest@gmail.com>
2023-11-17 09:22:13 +0700

    feat: Portfolio page for tablet screen

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,21 +3,32 @@ import porto1 from "../../public/img/1710.jpg";
 
 const Portfolio = () => {
 	return (
-		<div className="bg-[#111] font-poppins">
-			<div className="w-full px-8 flex justify-between items-center h-16 bg-[#252525]">
+		<div className="bg-[#111] font-poppins min-[577px]:pt-20">
+			<div className="w-full min-[577px]:hidden px-8 flex justify-between items-center h-16 bg-[#252525]">
 				<div className="text-3xl font-bold text-white">
 					MY <span className="text-[#FFB400]">PORTFOLIO</span>
 				</div>
 				<img className=" w-7" src={menu} alt="" />
 			</div>
+
+			<div className="absolute hidden min-[577px]:flex transition-all w-[53px] h-[53px] rounded-[6px] bg-[#252525] right-8 top-8 items-center justify-center">
+				<img className="w-7" src={menu} alt="" />
+			</div>
+
+			<div className="hidden min-[577px]:block text-[120px] relative font-bold text-[#222222] text-center">
+				WORKS
+				<div className="text-[56px] absolute w-[500px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 font-bold text-white">
+					MY <span className="text-[#FFB400] ">PORTFOLIO</span>
+				</div>
+			</div>
             
-            <div className="p-8">
+            <div className="p-8 min-[577px]:grid min-[577px]:grid-cols-2 gap-8">
                 <img className="rounded-lg" src={porto1} alt="" />
-                <img className="rounded-lg mt-8" src={porto1} alt="" />
-                <img className="rounded-lg mt-8" src={porto1} alt="" />
-                <img className="rounded-lg mt-8" src={porto1} alt="" />
-                <img className="rounded-lg mt-8" src={porto1} alt="" />
-                <img className="rounded-lg mt-8" src={porto1} alt="" />
+                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
+                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
+                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
+                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
+                <img className="rounded-lg mt-8 min-[577px]:mt-0" src={porto1} alt="" />
             </div>
 		</div>
 	);
